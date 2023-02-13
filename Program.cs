145e6367145ef5e4358583b2830ada56a83d5a71
Arun Babumon <arun.babumon@experionglobal.local>
2023-02-13 15:37:05 +0530

    Successful

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NewProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entert the first number");
+           var c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entert the Second  number");
+            var v = int.Parse(Console.ReadLine());
+            var tem = 0;
+            tem = c;
+            c = v;
+            v = tem;
+            Console.WriteLine("c = "+c+" v = "+v);
+        }
+    }
+}
